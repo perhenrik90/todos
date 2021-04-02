410d11c717b243e9cb8e7b21baaf431ab8e43ee6
Per-Henrik <perhenrik.kvalnes@gmail.com>
2021-04-02 20:23:02 +0200

    added database tabel for toto item for one todo entry

--- python/models.py
+++ python/models.py
@@ -1,7 +1,10 @@
 import datetime
+import os
 
 class Todo:
-
+    """
+    Describes a Todo-item
+    """
     def __init__(self, label):
 
         self.label = label
@@ -9,3 +12,17 @@ class Todo:
 
     def __str__(self):
         return self.label +' '+self.created.strftime("%Y-%m-%d")
+
+    
+    @staticmethod
+    def schema():
+        """
+        Return Postgres database table schema
+        """
+        return """
+        CREATE TABLE todo (
+          id SERIAL PRIMARY KEY,
+          label VARCHAR(80),
+          created TIMESTAMP DEFAULT NOW()
+        )
+        """
