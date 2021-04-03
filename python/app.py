f31bcfb1e20ea7ac91a6a5c27fd439d076fd65a6
Per-Henrik <perhenrik.kvalnes@gmail.com>
2021-04-03 09:48:10 +0200

    added http post for todo in Python

--- python/app.py
+++ python/app.py
@@ -9,12 +9,18 @@ app = flask.Flask(__name__)
 database.initDatabase()
 print("App started")
 
-m1 = models.Todo('Test 4')
-m1.save()
-
-
 @app.route('/todo', methods=['GET','POST'])
-def get_todos():
+def todo():
+    # IF get request, retrive todos
     if request.method == 'GET':
         objs = models.Todo.getTodos()
         return jsonify(objs)
+
+    # If post, create a new Todo post
+    if request.method == 'POST':
+        print("Post a new todo")
+        body = request.get_json(force=True)
+
+        m1 = models.Todo( body['label'] )
+        m1.save()
+        return jsonify({'message':'success'})
