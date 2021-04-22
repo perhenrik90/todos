fe8b9bdc958992c7898d3b1b3a1adcc32cacb987
Per-Henrik <perhenrik.kvalnes@gmail.com>
2021-04-22 22:14:10 +0200

    added app field in database

--- python/models.py
+++ python/models.py
@@ -18,7 +18,7 @@ class Todo:
 
     def save(self):
         con = database.connectionString()
-        q = "INSERT INTO todo (label) VALUES ('{}') RETURNING *;".format(self.label)
+        q = "INSERT INTO todo (label, app) VALUES ('{}', 'Python') RETURNING *;".format(self.label)
         con.cursor().execute(q)
         result = con.commit()
         print(result)
@@ -26,12 +26,12 @@ class Todo:
     @staticmethod
     def getTodosJSON():
         con = database.connectionString()
-        q = "SELECT id, label, x_pos,y_pos, created FROM todo;"
+        q = "SELECT id, label, app, x_pos,y_pos, created FROM todo;"
         cur = con.cursor()
         cur.execute(q)
         todos = []
         for r in cur.fetchall():
-            todos += [{'id':r[0], 'label':r[1],'x_pos':r[2],'y_pos':r[3], 'created':r[4]}]
+            todos += [{'id':r[0], 'label':r[1],'app':r[2],'x_pos':r[3],'y_pos':r[4], 'created':r[5]}]
         return todos
     
     @staticmethod
@@ -44,6 +44,7 @@ class Todo:
           id SERIAL PRIMARY KEY,
           label VARCHAR(80),
           created TIMESTAMP DEFAULT NOW(),
+          app VARCHAR(10),
           x_pos INTEGER DEFAULT 0,
           y_pos INTEGER DEFAULT 0
         )
