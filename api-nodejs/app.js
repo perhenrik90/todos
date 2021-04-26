6fe9f69ec573547afb7eb49d4e00b0c4718b99c8
Per-Henrik Kvalnes <perhenrik.kvalnes@gmail.com>
2021-04-26 20:11:55 +0200

    added GET http-method in nodejs restapi and postgres package

--- api-nodejs/app.js
+++ api-nodejs/app.js
@@ -1,10 +1,20 @@
+const { Pool, Client } = require('pg')
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/api_nodejs/todo', (req,res) => {
-    todos = [{"label":"API not implemented yet!"}]
-    res.json(todos);
+app.get('/api_nodejs/todo', async (req,res) => {
+    const c = new Client();
+    await c.connect();
+
+    if (req.method == "GET"){
+	const res_pg = await c.query("SELECT id,label, app,x_pos,y_pos FROM todo;")
+	res.json(res_pg.rows);
+    }
+    if (req.method == "POST"){
+	console.log( req.body );
+	res.json( req.body );
+    }
 });
 
 
