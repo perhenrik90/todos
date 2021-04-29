68f597ead9c3fea7f1b4e0690043f3fe021a859e
Per-Henrik Kvalnes <perhenrik.kvalnes@gmail.com>
2021-04-29 21:18:30 +0200

    Added first version of POST in nodejs

--- api-nodejs/app.js
+++ api-nodejs/app.js
@@ -1,21 +1,32 @@
 const { Pool, Client } = require('pg')
+
 const express = require('express');
 const app = express();
+
 const port = 3000;
+
+const bp = require('body-parser')
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
+
 
 app.get('/api_nodejs/todo', async (req,res) => {
     const c = new Client();
     await c.connect();
-
-    if (req.method == "GET"){
-	const res_pg = await c.query("SELECT id,label, app,x_pos,y_pos FROM todo;")
-	res.json(res_pg.rows);
-    }
-    if (req.method == "POST"){
-	console.log( req.body );
-	res.json( req.body );
-    }
+    const res_pg = await c.query("SELECT id,label, app,x_pos,y_pos FROM todo;")
+    res.json(res_pg.rows);
 });
+
+app.post('/api_nodejs/todo', async (req,res) => {
+
+    const c = new Client();
+    await c.connect();
+    sql = "INSERT INTO todo (label, app, x_pos,y_pos) VALUES ('"+req.body["label"]+"', 'JavaScript/Node', 1,1) RETURNING *;"
+    console.log( sql );
+    const res_pg = await c.query(sql)
+    res.json( req.body );
+});
+
 
 
 app.listen(port, () =>{
