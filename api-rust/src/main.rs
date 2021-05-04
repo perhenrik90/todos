33c4a54f41ff35c55586bc0fb2312867cfd91ab5
Per-Henrik Kvalnes <perhenrik.kvalnes@gmail.com>
2021-05-04 19:47:56 +0200

    added postgres and valid JSON response from rust API

--- api-rust/src/main.rs
+++ api-rust/src/main.rs
@@ -1,13 +1,15 @@
 #![feature(proc_macro_hygiene, decl_macro)]
-
 #[macro_use] extern crate rocket;
 
+use rocket::response::content;
+
+
 #[get("/")]
-fn todos() -> &'static str {
-   "HEllo rust!"
+fn todos() -> content::Json<&'static str> {
+   content::Json("[{\"label\":\"Hello Rust\", \"x_pos\":42, \"y_pos\":42}]")
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![todos]).launch();
+    rocket::ignite().mount("/api_rust/", routes![todos]).launch();
     
 }
